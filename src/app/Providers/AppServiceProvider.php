9b50df6a4332eb376694358b6d582d6fc1d65afc
46maru <c0b2108596@edu.teu.ac.jp>
2024-05-26 15:41:54 +0900

    修正

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,8 +15,14 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-    
-    public function boot(){
-        Paginator::useBootstrap();
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +13,6 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
-Route::get('/', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, "create"]);
-Route::get('/posts/{post}', [PostController::class, "show"]);
-Route::post('/posts', [PostController::class, "store"]);
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'delete']);
-Route::get('/categories/{category}', [CategoryController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
